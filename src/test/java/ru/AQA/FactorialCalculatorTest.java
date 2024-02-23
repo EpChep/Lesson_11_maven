370b8e861d7d7111f05e8701aa1ac27d395787c2
Eprem Chepnyan <156126721+EpChep@users.noreply.github.com>
2024-02-24 01:13:10 +0300

    Commit

--- src/test/java/ru/AQA/FactorialCalculatorTest.java
+++ src/test/java/ru/AQA/FactorialCalculatorTest.java
@@ -1,26 +1,27 @@
 package ru.AQA;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class FactorialCalculatorTest {
 
     @Test
-    void testFactorialOfZero() {
-        assertEquals(1, FactorialCalculator.factorial(0));
+    public void testFactorialOfZero() {
+        Assert.assertEquals(FactorialCalculator.factorial(0), 1);
     }
 
     @Test
-    void testFactorialOfPositiveNumber() {
-        assertEquals(1, FactorialCalculator.factorial(1));
-        assertEquals(2, FactorialCalculator.factorial(2));
-        assertEquals(6, FactorialCalculator.factorial(3));
-        assertEquals(24, FactorialCalculator.factorial(4));
-        assertEquals(120, FactorialCalculator.factorial(5));
+    public void testFactorialOfPositiveNumber() {
+        Assert.assertEquals(FactorialCalculator.factorial(1), 1);
+        Assert.assertEquals(FactorialCalculator.factorial(2), 2);
+        Assert.assertEquals(FactorialCalculator.factorial(3), 6);
+        Assert.assertEquals(FactorialCalculator.factorial(4), 24);
+        Assert.assertEquals(FactorialCalculator.factorial(5), 120);
     }
 
-    @Test
-    void testFactorialOfNegativeNumber() {
-        assertThrows(IllegalArgumentException.class, () -> FactorialCalculator.factorial(-1));
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testFactorialOfNegativeNumber() {
+        FactorialCalculator.factorial(-1);
     }
 }
+
